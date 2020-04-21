b98b3b624fcf9877a767f524d5421dbb986c1f44
Sebastian Alvarez <sleam91@gmail.com>
2020-04-21 20:38:40 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,46 @@
+const data = { "results": [{ "name": { "title": "Miss", "first": "Jasmine", "last": "Singh" }, "email": "Jasmine.Singh@example.com", "phone": "769-904-6220", "picture": { "large": "https://randomuser.me/api/portraits/women/85.jpg", "medium": "https://randomuser.me/api/portraits/med/women/85.jpg", "thumbnail": "https://randomuser.me/api/portraits/thumb/women/85.jpg" } }, { "name": { "title": "Mrs", "first": "Chloe", "last": "Zhang" }, "email": "Chloe.Zhang@example.com", "phone": "(995)-968-0405", "picture": { "large": "https://randomuser.me/api/portraits/women/93.jpg", "medium": "https://randomuser.me/api/portraits/med/women/93.jpg", "thumbnail": "https://randomuser.me/api/portraits/thumb/women/93.jpg" } }, { "name": { "title": "Mr", "first": "عباس", "last": "زارعی" }, "email": "aabs.zraay@example.com", "phone": "066-28761041", "picture": { "large": "https://randomuser.me/api/portraits/men/37.jpg", "medium": "https://randomuser.me/api/portraits/med/men/37.jpg", "thumbnail": "https://randomuser.me/api/portraits/thumb/men/37.jpg" } }, { "name": { "title": "Mrs", "first": "Pinja", "last": "Autio" }, "email": "Pinja.Autio@example.com", "phone": "03-164-484", "picture": { "large": "https://randomuser.me/api/portraits/women/5.jpg", "medium": "https://randomuser.me/api/portraits/med/women/5.jpg", "thumbnail": "https://randomuser.me/api/portraits/thumb/women/5.jpg" } }, { "name": { "title": "Miss", "first": "Rachel", "last": "Lemaire" }, "email": "Rachel.Lemaire@example.com", "phone": "02-91-22-31-25", "picture": { "large": "https://randomuser.me/api/portraits/women/44.jpg", "medium": "https://randomuser.me/api/portraits/med/women/44.jpg", "thumbnail": "https://randomuser.me/api/portraits/thumb/women/44.jpg" } }, { "name": { "title": "Miss", "first": "Viivi", "last": "Mikkola" }, "email": "Viivi.Mikkola@example.com", "phone": "03-675-390", "picture": { "large": "https://randomuser.me/api/portraits/women/64.jpg", "medium": "https://randomuser.me/api/portraits/med/women/64.jpg", "thumbnail": "https://randomuser.me/api/portraits/thumb/women/64.jpg" } }, { "name": { "title": "Ms", "first": "آوا", "last": "مرادی" }, "email": "aw.mrdy@example.com", "phone": "069-22294050", "picture": { "large": "https://randomuser.me/api/portraits/women/51.jpg", "medium": "https://randomuser.me/api/portraits/med/women/51.jpg", "thumbnail": "https://randomuser.me/api/portraits/thumb/women/51.jpg" } }, { "name": { "title": "Mr", "first": "Eckhart", "last": "Kirsten" }, "email": "Eckhart.Kirsten@example.com", "phone": "0749-3852718", "picture": { "large": "https://randomuser.me/api/portraits/men/34.jpg", "medium": "https://randomuser.me/api/portraits/med/men/34.jpg", "thumbnail": "https://randomuser.me/api/portraits/thumb/men/34.jpg" } }, { "name": { "title": "Mr", "first": "Birkan", "last": "Van der Stroom" }, "email": "Birkan.VanderStroom@example.com", "phone": "(183)-737-2106", "picture": { "large": "https://randomuser.me/api/portraits/men/63.jpg", "medium": "https://randomuser.me/api/portraits/med/men/63.jpg", "thumbnail": "https://randomuser.me/api/portraits/thumb/men/63.jpg" } }, { "name": { "title": "Mr", "first": "Dan", "last": "Niebuhr" }, "email": "Dan.Niebuhr@example.com", "phone": "0569-7016684", "picture": { "large": "https://randomuser.me/api/portraits/men/85.jpg", "medium": "https://randomuser.me/api/portraits/med/men/85.jpg", "thumbnail": "https://randomuser.me/api/portraits/thumb/men/85.jpg" } }, { "name": { "title": "Mrs", "first": "Andrea", "last": "Mckinney" }, "email": "Andrea.Mckinney@example.com", "phone": "071-502-1842", "picture": { "large": "https://randomuser.me/api/portraits/women/40.jpg", "medium": "https://randomuser.me/api/portraits/med/women/40.jpg", "thumbnail": "https://randomuser.me/api/portraits/thumb/women/40.jpg" } }, { "name": { "title": "Mr", "first": "Derrick", "last": "Weaver" }, "email": "Derrick.Weaver@example.com", "phone": "(711)-884-5832", "picture": { "large": "https://randomuser.me/api/portraits/men/91.jpg", "medium": "https://randomuser.me/api/portraits/med/men/91.jpg", "thumbnail": "https://randomuser.me/api/portraits/thumb/men/91.jpg" } }, { "name": { "title": "Madame", "first": "Léa", "last": "Simon" }, "email": "Lea.Simon@example.com", "phone": "077 320 47 89", "picture": { "large": "https://randomuser.me/api/portraits/women/50.jpg", "medium": "https://randomuser.me/api/portraits/med/women/50.jpg", "thumbnail": "https://randomuser.me/api/portraits/thumb/women/50.jpg" } }, { "name": { "title": "Mr", "first": "Raimundo", "last": "Barros" }, "email": "Raimundo.Barros@example.com", "phone": "(55) 7965-7321", "picture": { "large": "https://randomuser.me/api/portraits/men/77.jpg", "medium": "https://randomuser.me/api/portraits/med/men/77.jpg", "thumbnail": "https://randomuser.me/api/portraits/thumb/men/77.jpg" } }, { "name": { "title": "Mr", "first": "Eren", "last": "Sözeri" }, "email": "Eren.Sozeri@example.com", "phone": "(132)-689-0006", "picture": { "large": "https://randomuser.me/api/portraits/men/47.jpg", "medium": "https://randomuser.me/api/portraits/med/men/47.jpg", "thumbnail": "https://randomuser.me/api/portraits/thumb/men/47.jpg" } }, { "name": { "title": "Miss", "first": "Coline", "last": "Noel" }, "email": "Coline.Noel@example.com", "phone": "01-89-44-13-44", "picture": { "large": "https://randomuser.me/api/portraits/women/61.jpg", "medium": "https://randomuser.me/api/portraits/med/women/61.jpg", "thumbnail": "https://randomuser.me/api/portraits/thumb/women/61.jpg" } }, { "name": { "title": "Monsieur", "first": "Carl", "last": "Giraud" }, "email": "Carl.Giraud@example.com", "phone": "076 376 36 50", "picture": { "large": "https://randomuser.me/api/portraits/men/37.jpg", "medium": "https://randomuser.me/api/portraits/med/men/37.jpg", "thumbnail": "https://randomuser.me/api/portraits/thumb/men/37.jpg" } }, { "name": { "title": "Ms", "first": "Sophie", "last": "Turner" }, "email": "Sophie.Turner@example.com", "phone": "(064)-788-4604", "picture": { "large": "https://randomuser.me/api/portraits/women/53.jpg", "medium": "https://randomuser.me/api/portraits/med/women/53.jpg", "thumbnail": "https://randomuser.me/api/portraits/thumb/women/53.jpg" } }, { "name": { "title": "Mr", "first": "Eric", "last": "Mai" }, "email": "Eric.Mai@example.com", "phone": "0869-2701517", "picture": { "large": "https://randomuser.me/api/portraits/men/89.jpg", "medium": "https://randomuser.me/api/portraits/med/men/89.jpg", "thumbnail": "https://randomuser.me/api/portraits/thumb/men/89.jpg" } }, { "name": { "title": "Miss", "first": "Rukiye", "last": "Van der Kruk" }, "email": "Rukiye.VanderKruk@example.com", "phone": "(996)-772-9008", "picture": { "large": "https://randomuser.me/api/portraits/women/67.jpg", "medium": "https://randomuser.me/api/portraits/med/women/67.jpg", "thumbnail": "https://randomuser.me/api/portraits/thumb/women/67.jpg" } }], "info": { "seed": "65d89dd7259a8619", "results": 20, "page": 1, "version": "1.3" } }
+const users = data.results
+
+let rightArrow = document.getElementsByClassName("right")[0]
+let name = document.getElementsByClassName("name")[0]
+let portrait = document.getElementsByClassName("portrait")[0]
+let email = document.getElementsByClassName("email")[0]
+let phone = document.getElementsByClassName("phone")[0]
+let leftArrow = document.getElementsByClassName("left")[0]
+
+let counter = 0
+let currentUser = users[0]
+let currentName = currentUser.name
+name.innerText = `${currentName.title} ${currentName.first} ${currentName.last}`
+portrait.setAttribute("src", currentUser.picture.large)
+email.innerText = currentUser.email
+phone.innerText = currentUser.phone
+
+
+
+rightArrow.addEventListener("click", event => {
+    if (counter === users.length-1) {
+        counter = -1
+    }
+    currentUser = users[++counter]
+    currentName = currentUser.name
+    name.innerText = `${currentName.title} ${currentName.first} ${currentName.last}`
+    portrait.setAttribute("src", currentUser.picture.large)
+    email.innerText = currentUser.email
+    phone.innerText = currentUser.phone
+
+})
+
+
+leftArrow.addEventListener("click", event => {
+    if (counter === 0) {
+        counter = users.length
+    }
+    currentUser = users[--counter]
+    currentName = currentUser.name
+    name.innerText = `${currentName.title} ${currentName.first} ${currentName.last}`
+    portrait.setAttribute("src", currentUser.picture.large)
+    email.innerText = currentUser.email
+    phone.innerText = currentUser.phone
+
+})
